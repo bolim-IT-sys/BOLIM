b1e0e1cdcd7495f2beaecbb70097c4431c122665
JemLlanto <105899347+JemLlanto@users.noreply.github.com>
2025-12-10 14:52:44 +0800

    Updated sorting in table.

--- client/src/helper/sorting.helper.tsx
+++ client/src/helper/sorting.helper.tsx
@@ -1,4 +1,10 @@
 import type { Part } from "../services/Part.Service";
+import {
+  computeExcessInsufficient,
+  computeOrderQuantity,
+  computeSecurementRate,
+  computeUrgentRequest,
+} from "./table.helper";
 
 export const sortByStocks = (parts: Part[], order: "asc" | "desc") => {
   return [...parts].sort((a, b) => {
@@ -28,6 +34,73 @@ export const sortByPrice = (parts: Part[], order: "asc" | "desc") => {
       order === "asc"
         ? Number(a.unitPrice) - Number(b.unitPrice)
         : Number(b.unitPrice) - Number(a.unitPrice);
+
+    //SORTING BY PARTNUMBER WHEN UNIT PRICE IS THE SAME
+    if (primary === 0) {
+      return a.partNumber.localeCompare(b.partNumber);
+    }
+
+    return primary;
+  });
+};
+
+export const sortBySecurementRate = (parts: Part[], order: "asc" | "desc") => {
+  return [...parts].sort((a, b) => {
+    const primary =
+      order === "asc"
+        ? computeSecurementRate(a) - computeSecurementRate(b)
+        : computeSecurementRate(b) - computeSecurementRate(a);
+
+    //SORTING BY PARTNUMBER WHEN UNIT PRICE IS THE SAME
+    if (primary === 0) {
+      return a.partNumber.localeCompare(b.partNumber);
+    }
+
+    return primary;
+  });
+};
+
+export const sortByExcessInsufficient = (
+  parts: Part[],
+  order: "asc" | "desc"
+) => {
+  return [...parts].sort((a, b) => {
+    const primary =
+      order === "asc"
+        ? computeExcessInsufficient(a) - computeExcessInsufficient(b)
+        : computeExcessInsufficient(b) - computeExcessInsufficient(a);
+
+    //SORTING BY PARTNUMBER WHEN UNIT PRICE IS THE SAME
+    if (primary === 0) {
+      return a.partNumber.localeCompare(b.partNumber);
+    }
+
+    return primary;
+  });
+};
+
+export const sortByUrgentRequest = (parts: Part[], order: "asc" | "desc") => {
+  return [...parts].sort((a, b) => {
+    const primary =
+      order === "asc"
+        ? computeUrgentRequest(a) - computeUrgentRequest(b)
+        : computeUrgentRequest(b) - computeUrgentRequest(a);
+
+    //SORTING BY PARTNUMBER WHEN UNIT PRICE IS THE SAME
+    if (primary === 0) {
+      return a.partNumber.localeCompare(b.partNumber);
+    }
+
+    return primary;
+  });
+};
+
+export const sortByOrderQuantity = (parts: Part[], order: "asc" | "desc") => {
+  return [...parts].sort((a, b) => {
+    const primary =
+      order === "asc"
+        ? computeOrderQuantity(a) - computeOrderQuantity(b)
+        : computeOrderQuantity(b) - computeOrderQuantity(a);
 
     //SORTING BY PARTNUMBER WHEN UNIT PRICE IS THE SAME
     if (primary === 0) {
