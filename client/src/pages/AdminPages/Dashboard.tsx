ed29bfb5ba2c385532ab416ff3b55db43eecce75
JemLlanto <105899347+JemLlanto@users.noreply.github.com>
2025-12-20 15:26:50 +0800

    fixed autosaving of dataType in localStorage

--- client/src/pages/AdminPages/Dashboard.tsx
+++ client/src/pages/AdminPages/Dashboard.tsx
@@ -21,6 +21,7 @@ import {
 } from "recharts";
 import { getDateRange } from "../../helper/date.helper";
 import type { User } from "../../services/User.Service";
+import { getDataType } from "../../helper/dashboard.helper";
 
 interface ContextType {
   user: User;
@@ -41,34 +42,22 @@ export default function Dashboard() {
       // console.log("No user yet, dataType remains empty");
       return; // or set a default: setDataType("Pins");
     }
-
-    const stored = localStorage.getItem("dataType");
-    // console.log(stored ? "Has Stored" : "Has no Stored");
-    if (stored) {
-      const parsedData = JSON.parse(stored);
-      // console.log("Parsed: ", parsedData);
-      if (parsedData === "Pins" && user.pins) {
-        setDataType("Pins");
-        // console.log("11INITIAL DATA TYPE: ", "Pins");
-      } else if (parsedData === "ITStocks" && user.it_stocks) {
-        setDataType("ITStocks");
-        // console.log("12INITIAL DATA TYPE: ", "ITStocks");
-      } else if (parsedData === "MaterialControl" && user.materials) {
-        setDataType("MaterialControl");
-        // console.log("13INITIAL DATA TYPE: ", "MaterialControl");
-      }
-      // console.log("1INITIAL DATA TYPE: ", JSON.parse(stored));
-    } else if (user.pins) {
-      setDataType("Pins");
-      // console.log("2INITIAL DATA TYPE: ", "Pins");
-    } else if (user.it_stocks) {
-      setDataType("ITStocks");
-      // console.log("3INITIAL DATA TYPE: ", "ITStocks");
-    } else if (user.materials) {
-      setDataType("MaterialControl");
-      // console.log("4INITIAL DATA TYPE: ", "MaterialControl");
-    }
+    setTimeout(() => {
+      getDataType(user, setDataType);
+    }, 100);
   }, [user]);
+
+  useEffect(() => {
+    if (user) {
+      if (!dataType || dataType === "") {
+        // console.log("data type cant be saved.");
+        // localStorage.removeItem("dataType");
+      } else {
+        // console.log("data type can be saved.");
+        localStorage.setItem("dataType", JSON.stringify(dataType));
+      }
+    }
+  }, [user, dataType]);
 
   const [data, setData] = useState<Part[]>([]);
 
@@ -84,19 +73,7 @@ export default function Dashboard() {
       // console.log("No data type.");
       setData([]);
     }
-  }, [user, dataType, parts, ITStocks, materials]);
-
-  useEffect(() => {
-    if (user) {
-      if (!dataType || dataType === "") {
-        // console.log("data type cant be saved.");
-        localStorage.removeItem("dataType");
-      } else {
-        // console.log("data type can be saved.");
-        localStorage.setItem("dataType", JSON.stringify(dataType));
-      }
-    }
-  }, [user, dataType]);
+  }, [dataType, parts, ITStocks, materials]);
 
   const [dateRange, setDateRange] = useState(() => {
     const stored = localStorage.getItem("dateRange");
